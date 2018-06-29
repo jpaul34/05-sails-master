bf42db51af73e2ebedaafb4afd6ef55c0a1e6472
Jonathan Caiza <jpaul.snp@gmail.com>
2018-06-29 17:45:45 -0500

    Initial commit

---
+++ raspberry/config/bootstrap.js
@@ -0,0 +1,39 @@
+/**
+ * Bootstrap
+ * (sails.config.bootstrap)
+ *
+ * An asynchronous bootstrap function that runs just before your Sails app gets lifted.
+ * > Need more flexibility?  You can also do this by creating a hook.
+ *
+ * For more information on bootstrapping your app, check out:
+ * https://sailsjs.com/config/bootstrap
+ */
+
+const request = require('request');
+
+module.exports.bootstrap = async function (done) {
+
+  setInterval(
+    () => {
+      console.log('Enviar datos');
+
+      const objetoRequest = {
+        url: 'http://localhost:1338/Pulsacion',
+        form: {
+          nombreRaspberry: 'Lolita',
+          usuarioIdFK: 16 // USEN EL SUYO
+        }
+      };
+      request
+        .post(objetoRequest,
+          (err, respuesta, body) => {
+            console.log('err', err);
+            console.log('respuesta', respuesta);
+            console.log('body', body);
+          })
+    }
+    , 10000);
+
+  return done();
+
+};
